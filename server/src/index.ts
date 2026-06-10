dfda5f6c747f4ab2a7b16c47c9d963f80c483483
Aiman Nadeem <s4095136@student.rmit.edu.au>
2026-06-10 19:37:18 +1000

    Updated index to use cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import apiRouter from './routes/api'
+import coachesRouter from './routes/coaches'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(express.json())
 app.use('/api', apiRouter)
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (_req, res) => {
   res.send({ status: 'ok', message: 'th-cricket server running' })
@@ -14,3 +19,5 @@ app.get('/', (_req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
+
+app.use('/api', coachesRouter)
